bd42d0da1bff7564f407cc97f4d193d71830e7ca
innovex <iannjoroge120@outlook.com>
2024-05-14 09:36:54 +0300

    added production server url

--- src/app/services/service.service.ts
+++ src/app/services/service.service.ts
@@ -2,33 +2,37 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Collection, Invoice, School, createINV } from '../models/model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ServicesService {
+  private apiUrl = environment.apiUrl;
+
   constructor(private http: HttpClient) {}
 
   getUpcomingInvoices(): Observable<Invoice[]> {
-    return this.http.get<Invoice[]>('http://localhost:3000/invoices');
+    return this.http.get<Invoice[]>(`${this.apiUrl}/invoices`);
   }
 
   getSchools(): Observable<School[]> {
-    return this.http.get<School[]>('http://localhost:3000/schools');
+    return this.http.get<School[]>(`${this.apiUrl}/schools`);
   }
 
   getCollections(): Observable<Collection[]> {
-    return this.http.get<Collection[]>('http://localhost:3000/collection');
+    return this.http.get<Collection[]>(`${this.apiUrl}/collection`);
   }
 
-  editSIngleCollection(id: string,patchdata:any): Observable<Collection> {
-    return this.http.patch<Collection>(`http://localhost:3000/collection/${id}`,patchdata);
+  editSIngleCollection(id: string, patchdata: any): Observable<Collection> {
+    return this.http.patch<Collection>(`${this.apiUrl}/collection/${id}`, patchdata);
   }
 
   createInvoice(invoice: createINV): Observable<Invoice> {
-    return this.http.post<Invoice>('http://localhost:3000/invoices', invoice);
+    return this.http.post<Invoice>(`${this.apiUrl}/invoices`, invoice);
   }
+
   deleteInvoice(id: string): Observable<Invoice[]> {
-    return this.http.delete<Invoice[]>(`http://localhost:3000/invoices/${id}`);
+    return this.http.delete<Invoice[]>(`${this.apiUrl}/invoices/${id}`);
   }
 }
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    apiUrl: 'https://my-json-server.typicode.com/lorcents/e-learning'
+  };
+  
